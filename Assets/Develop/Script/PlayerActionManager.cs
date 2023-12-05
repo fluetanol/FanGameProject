24cf12e092238d9ca6c26203e61032f74938f5ab
fluetanol <mike415415@naver.com>
2023-12-05 16:06:24 +0900

    Feat) 외곽선 쉐이더 제작 및 2D오브젝트에 대한 이미션 쉐이더 제작
    외곽선 쉐이더 제작 및 2D오브젝트에 대한 이미션 쉐이더 제작
    머터리얼로 변환해서 적용했습니다

--- Assets/Develop/Script/PlayerActionManager.cs
+++ Assets/Develop/Script/PlayerActionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,26 +7,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerActionManager : MonoBehaviour
 {
-    Vector3 moveDirection;
-    int moveSpeed = 2;
+    [SerializeField] private int _moveSpeed = 5;
+    [SerializeField] private int _jumpForce = 5;
+    [SerializeField] private Rigidbody2D rigidbody2d;
+    private Vector3 _moveDirection;
+    private bool _isLand = false;
 
+    void Start(){
+    }
 
     void Update()
     {
-        bool hasControl = (moveDirection != Vector3.zero);
-        if (hasControl)
-        {
-            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        bool hasControl = (_moveDirection != Vector3.zero);
+        if (hasControl) {
+            rigidbody2d.AddForce(_moveDirection * _moveSpeed, ForceMode2D.Force);
         }
+
+    }
+
+    void FixedUpdate() {
+        if (!_isLand && rigidbody2d.GetPointVelocity(transform.position) == Vector2.zero) _isLand = true;
+        print(rigidbody2d.GetPointVelocity(transform.position));
     }
     
+    //Player Input Class Send Message
     void OnMove(InputValue value){
-
         Vector2 input = value.Get<Vector2>();
         if(input!=null){
-            
-            moveDirection = new Vector2(input.x, input.y);
+            _moveDirection = new Vector2(input.x, input.y);
             Debug.Log($"Send: {input.magnitude}, input : {input.x}, {input.y}");
+        }
+    }
+
+    void OnJump(InputValue value){
+        float input = value.Get<float>();
+        if(input==1 && _isLand){
+            rigidbody2d.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _isLand = false;
         }
     }
 
